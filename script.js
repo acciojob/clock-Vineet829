d63962c97adb38e489874ec69eeb9a0a947e90ce
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-09-29 16:00:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+let date = new Date()
+let time = document.querySelector("#timer")
+let text = document.createElement("p")
+let options = {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true
+};
+
+let formattedDate = date.toLocaleString('en-US', options);
+
+text.textContent = formattedDate
+ time.appendChild(text)
+
